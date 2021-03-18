2d0d59a03194d58f4a57b90e8f5be57515a496c2
Abhishek <55610756+darkhunter12@users.noreply.github.com>
2021-03-18 10:51:05 +0530

    Update user.js

--- src/models/user.js
+++ src/models/user.js
@@ -8,14 +8,17 @@ const UserSchema = new mongoose.Schema({
     password:String
 
 }) ;
+UserSchema.plugin(passportLocalMongoose);
+module.exports = mongoose.model("User",UserSchema);
 
 
-/*Hash password before saving
+
+
+/*
+//Hash password before saving
 UserSchema.pre('save', function(next) {
     var user = this
-  
-    if ( !user.isModified('password') ) return next()
-  
+  //  if ( !user.isModified('password') ) return next()
     bcrypt.hash(user.password, 10, (err, hash) => {
       if (err) {
         return next(err)
@@ -24,8 +27,7 @@ UserSchema.pre('save', function(next) {
       next()
     })
   })
-
-  /* Password verification
+  // Password verification
 UserSchema.methods.login = function(password) {
   var user = this
   return new Promise((resolve, reject) => {
@@ -37,6 +39,39 @@ UserSchema.methods.login = function(password) {
 }
 */
 
-UserSchema.plugin(passportLocalMongoose);
-module.exports = mongoose.model("User",UserSchema);
+
+
+==================================================================
+/*
+UserSchema.pre('save',async function(next) {
+try{
+  const salt = await bcrypt.genSalt(10)
+  const hashPassword = await bcrypt.hash(this.password, salt);
+  console.log(hashPassword)
+  this.password = hashPassword
+  next()
+}catch(err){
+  next(err)
+}
+})
+=================================================================
+  bcrypt.compare(password, user.password, (err, isMatch) => {
+      if (err) {
+        throw err
+      } else if (!isMatch) {
+        console.log("Password doesn't match!")
+        return done();
+      } else {
+        console.log("Password matches!")
+      }
+    }) 
+=================================================================
+     if (user.password !== password){
+        alert("password is incorrect......");
+        console.log("password is incorrect");
+        return done();
+      }
+*/
+
+
 
